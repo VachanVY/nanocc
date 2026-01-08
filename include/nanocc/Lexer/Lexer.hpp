567b5e69e2f0d41307890b436cf5ef132e419c4c
Vachan V Y <vachanvy05@gmail.com>
2026-01-09 00:58:48 +0530

    draft to be tested
    - Pure C implemetation: Refactor lexer implementation to use finite state machines and remove regex dependencies

--- include/nanocc/Lexer/Lexer.hpp
+++ include/nanocc/Lexer/Lexer.hpp
@@ -1,57 +1,12 @@
 #pragma once
 
 #include <string>
-#include <regex>
-
-#define TOKEN_LIST                                                                                 \
-    X(INT, "int")                                                                                  \
-    X(VOID, "void")                                                                                \
-    X(RETURN, "return")                                                                            \
-    X(IF, "if")                                                                                    \
-    X(ELSE, "else")                                                                                \
-    X(QUESTION, "?")                                                                               \
-    X(COLON, ":")                                                                                  \
-    X(DO, "do")                                                                                    \
-    X(WHILE, "while")                                                                              \
-    X(FOR, "for")                                                                                  \
-    X(BREAK, "break")                                                                              \
-    X(CONTINUE, "continue")                                                                        \
-                                                                                                   \
-    X(TILDE, "~")                                                                                  \
-    X(DECREMENT, "--")                                                                             \
-    X(NOT, "!")                                                                                    \
-                                                                                                   \
-    X(MINUS, "-")                                                                                  \
-                                                                                                   \
-    X(PLUS, "+")                                                                                   \
-    X(STAR, "*")                                                                                   \
-    X(SLASH, "/")                                                                                  \
-    X(PERCENT, "%")                                                                                \
-    X(AND, "&&")                                                                                   \
-    X(OR, "||")                                                                                    \
-                                                                                                   \
-    X(ASSIGN, "=")                                                                                 \
-                                                                                                   \
-    X(EQUAL, "==")                                                                                 \
-    X(NOT_EQUAL, "!=")                                                                             \
-    X(LESSTHAN, "<")                                                                               \
-    X(GREATERTHAN, ">")                                                                            \
-    X(LESS_EQUAL, "<=")                                                                            \
-    X(GREATER_EQUAL, ">=")                                                                         \
-                                                                                                   \
-    X(IDENTIFIER, "identifier")                                                                    \
-    X(CONSTANT, "constant")                                                                        \
-    X(LPAREN, "(")                                                                                 \
-    X(RPAREN, ")")                                                                                 \
-    X(LBRACE, "{")                                                                                 \
-    X(RBRACE, "}")                                                                                 \
-    X(SEMICOLON, ";")                                                                              \
-                                                                                                   \
-    X(COMMA, ",")
+#include <deque>
+#include <stdexcept>
 
 enum class TokenType {
 #define X(name, str) name,
-    TOKEN_LIST
+    #include "../../../lib/Lexer/CAPI/tokens.def"
 #undef X
 };
 
@@ -60,14 +15,12 @@ struct Token {
     std::string lexeme; // source text matched for that token
 };
 
-extern std::pair<TokenType, std::regex> token_specs[];
-
 constexpr std::string_view tokenTypeToString(TokenType type) {
     switch (type) {
 #define X(name, str)                                                                               \
     case TokenType::name:                                                                          \
         return str;
-        TOKEN_LIST
+    #include "../../../lib/Lexer/CAPI/tokens.def"
 #undef X
     }
     throw std::runtime_error("Unknown TokenType");
@@ -78,5 +31,5 @@ namespace nanocc {
 /// @param s The source code string to analyze.
 /// @param debug Whether to print debug information during lexing.
 /// @return A deque of tokens generated from the source code.
-std::deque<Token> lexer(const std::string& s, bool debug = false);
+std::deque<Token> lexer(std::string& s, bool debug = false);
 } // namespace nanocc
---
+++ lib/Lexer/CAPI/regex.h
@@ -0,0 +1,58 @@
+#pragma once
+
+int int_fsm(const char* s);
+int void_fsm(const char* s);
+int return_fsm(const char* s);
+int if_fsm(const char* s);
+int else_fsm(const char* s);
+int question_fsm(const char* s);
+int colon_fsm(const char* s);
+int do_fsm(const char* s);
+int while_fsm(const char* s);
+int for_fsm(const char* s);
+int break_fsm(const char* s);
+int continue_fsm(const char* s);
+
+// "unary operators"
+int tilde_fsm(const char* s);
+int decrement_fsm(const char* s);
+int not_fsm(const char* s);
+
+// both unary and binary
+int minus_fsm(const char* s);
+
+// "binary operators"
+int plus_fsm(const char* s);
+int star_fsm(const char* s);
+int slash_fsm(const char* s);
+int percent_fsm(const char* s);
+int and_fsm(const char* s);
+int or_fsm(const char* s);
+
+// assignment operators // comes under binary operators but has less precedence
+int assign_fsm(const char* s);
+
+int equal_fsm(const char* s);
+int not_equal_fsm(const char* s);
+int lessthan_fsm(const char* s);
+int greaterthan_fsm(const char* s);
+int less_equal_fsm(const char* s);
+int greater_equal_fsm(const char* s);
+
+/// @brief `std::regex("[a-zA-Z_]\\w*\\b")` | variable/function names
+/// @param s
+/// @return match length if it matches else 0
+int identifier_fsm(const char* s);
+
+/// @brief `std::regex("[0-9]+\\b")`
+/// @param s
+/// @return match length if it matches else 0
+int constant_fsm(const char* s);
+int lparen_fsm(const char* s);
+int rparen_fsm(const char* s);
+int lbrace_fsm(const char* s);
+int rbrace_fsm(const char* s);
+int semicolon_fsm(const char* s);
+
+// function args separator
+int comma_fsm(const char* s);
