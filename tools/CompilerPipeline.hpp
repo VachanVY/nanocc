235ca388aeeb83eb0dd4762f9cda39ba764d3ce0
Vachan V Y <vachanvy05@gmail.com>
2026-03-20 23:47:46 +0530

    add Transforms into build system; update CompilerPipeline for optimization

--- tools/CompilerPipeline.hpp
+++ tools/CompilerPipeline.hpp
@@ -7,15 +7,19 @@
 #include "nanocc/Parser/Parser.hpp"
 #include "nanocc/Sema/Sema.hpp"
 #include "nanocc/Target/X86/X86TargetEmitter.hpp"
+#include "nanocc/Transforms/PassManager.hpp"
 #include "nanocc/Utils/Utils.hpp"
 
 std::string getAsmOutputFromCFile(const std::string &c_filename,
-                                  bool debug = false) {
+                                  bool debug = false, bool optimize = false) {
   auto contents = nanocc::getFileContents(c_filename);
   auto tokens = nanocc::lexer(contents, debug);
   auto ast = nanocc::parse(tokens, debug);
   nanocc::semanticAnalysis(ast, debug);
   auto interm_repr = nanocc::generateIntermRepr(ast, debug);
+  if (optimize) {
+    nanocc::runIROptimizationPipeline(*interm_repr, {});
+  }
   auto pseudo_asm = nanocc::intermReprToPseudoAsm(interm_repr, debug);
   nanocc::x86CorrectAssembly(pseudo_asm, debug);
   auto output = nanocc::x86EmitAssembly(pseudo_asm);
