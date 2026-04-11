33f6c7fe6da2f897016c5f7ba85ba204d9fad4af
VachanVY <vachanvy05@gmail.com>
2026-04-11 14:46:35 +0530

    CopyPropagate and DeadStoreElimination function: Yet to be implemented

--- lib/Transforms/CopyPropagation.cpp
+++ lib/Transforms/CopyPropagation.cpp
@@ -2,6 +2,8 @@
 #include "nanocc/IR/IR.hpp"
 
 namespace nanocc {
-
+/// @brief Yet to be implemented
+/// @param IRProgram 
+/// @return 
 bool CopyPropagate(IRProgramNode& IRProgram) { return false; }
 } // namespace nanocc
--- lib/Transforms/DeadStoreElimination.cpp
+++ lib/Transforms/DeadStoreElimination.cpp
@@ -2,5 +2,8 @@
 #include "nanocc/IR/IR.hpp"
 
 namespace nanocc {
+/// @brief Yet to be implemented
+/// @param IRProgram 
+/// @return 
 bool DeadStoreElimination(IRProgramNode& IRProgram) { return false; }
 } // namespace nanocc
